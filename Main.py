5d7d204c04d22f32cbb1737a51d2271938b360bf
Alex Habich <me@alexnader.dev>
2021-02-27 15:58:53 -0600

    Update main

--- Main.py
+++ Main.py
@@ -6,32 +6,35 @@ from discord.ext import commands
 
 def main():
 	# Bot initialization
-	client = Bot.commands(command_prefix="UTD")
+	client = commands.Bot('UTD')
 	client.remove_command("help")
 
 	# Help command: Update as needed
 	@client.command()
 	async def help(ctx):
-		help = discord.embed(
-			colour=discord.colour(4359413),
-			title="Help",
-			description="THE place for managing your UTD courses. Below is a list of commands.")
-		await ctx.send(embed=help)
+		await ctx.send(embed=discord.Embed(
+			color=4359413,
+			title='Help',
+			description='THE place for managing your UTD courses. Below is a list of commands.'
+		))
 
 	# Loads commands from other python files
-	@client.command()
+	@client.command('load')
 	async def load_extension(ctx, file):
 		client.load_extension(f"cogs.{file}")
 
 	# Removes commands from other python files
-	@client.command()
+	@client.command('unload')
 	async def unload_extension(ctx, file):
 		client.unload_extension(f"cogs.{file}")
 
-		# Reads and creates commands from files listed in the cogs folder
-		for file in os.listdir("./cogs"):
-			if file.endswith(".py"):
+	# Reads and creates commands from files listed in the cogs folder
+	for file in os.listdir("./cogs"):
+		if file.endswith(".py"):
+			try:
 				client.load_extension(f"cogs.{file[:-3]}")
+			except:
+				print(f'failed to load cog {file}')
 
 	# Load environment variable (Token)
 	load_dotenv()
