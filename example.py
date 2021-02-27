801c160baa2e5dd1d29f85ef3dfe456561efe61e
Huy Nguyen <73027756+huyaboo@users.noreply.github.com>
2021-02-27 14:18:50 -0600

    Update example.py

--- example.py
+++ example.py
@@ -3,13 +3,14 @@ from discord.ext import commands
 
 #EXAMPLE FORMAT OF THE FILE TO PLACE YOUR COMMANDS
 class Classname(commands.Cog):
+	#Creates class in which to place commands
 	def __init__(self, client):
 		self.client = client
 
-	#PUT THIS FOR EVENTS
+	#PUT THIS FOR EVENTS (replaces @client.event())
 	@commands.Cog.listener()
 
-	#PUT THIS ABOVE ANY COMMAND Y'ALL WANNA MAKE
+	#PUT THIS ABOVE ANY COMMAND Y'ALL WANNA MAKE (replaces @client.command())
 	@commands.command()
 
 #PUT THIS AT THE END OF YOUR PYTHON FILE
